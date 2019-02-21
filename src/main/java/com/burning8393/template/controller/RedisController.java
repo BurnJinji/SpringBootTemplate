51badb260b0530a6ae11744b91aee06b865dcce7
pangxw <pang798763716@outlook.com>
2019-02-21 23:47:59 +0800

    Redis集成

---
+++ src/main/java/com/burning8393/template/controller/RedisController.java
@@ -0,0 +1,34 @@
+package com.burning8393.template.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Pang Xiaowei
+ * @title: RedisController
+ * @projectName template
+ * @description: TODO
+ * @date 2019-02-2123:25
+ */
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("set/{key}/{value}")
+    public String set(@PathVariable("key") String key, @PathVariable("value") String value) {
+        redisTemplate.opsForValue().set(key, value);
+        return key + ", " + value;
+    }
+
+    @GetMapping("get/{key}")
+    public String get(@PathVariable("key") String key) {
+        return "key=" + key + ", value = " + redisTemplate.opsForValue().get(key);
+    }
+}
