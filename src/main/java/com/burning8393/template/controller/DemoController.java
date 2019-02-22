bbcf7fc1faa07fabfcc6cdef2218669b952d6870
pangxw <pang798763716@outlook.com>
2019-02-22 22:39:13 +0800

    rabbitmq 测试

--- src/main/java/com/burning8393/template/controller/DemoController.java
+++ src/main/java/com/burning8393/template/controller/DemoController.java
@@ -2,9 +2,8 @@ package com.burning8393.template.controller;
 
 import com.burning8393.template.config.Config;
 import com.burning8393.template.model.DemoReq;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +20,6 @@ import javax.validation.Valid;
  * @date 2019-02-2022:41
  */
 @Slf4j
-@Api(tags = "Demo API")
 @RestController
 public class DemoController {
 
@@ -35,6 +33,9 @@ public class DemoController {
     String desc;
 
     @Autowired
+    AmqpTemplate rabbitmqTemplate;
+
+    @Autowired
     Config config;
     @GetMapping("/demo")
     public String demo() {
@@ -42,19 +43,16 @@ public class DemoController {
         return "hello SpringBoot";
     }
 
-    @ApiOperation("demo描述")
     @GetMapping("/desc")
     public String desc() {
         return desc;
     }
 
-    @ApiOperation("demo 配置")
     @GetMapping("/config")
     public Config config() {
         return config;
     }
 
-    @ApiOperation("demo 登陆")
     @GetMapping("/testListenerLogin")
     public String testListenerLogin(HttpServletRequest request) {
         log.info("当前在线人数： " + request.getSession().getId() + ": "
@@ -62,15 +60,20 @@ public class DemoController {
         return "Hello testListenerLogin";
     }
 
-    @ApiOperation("demo 测试异常")
     @GetMapping("/testExceptionHandler")
     public String testExceptionHandler() {
         throw new RuntimeException("testExceptionHandler");
     }
 
-    @ApiOperation("demo 测试验证效果")
     @GetMapping("/demo/valid")
     public String demoValid(@Valid DemoReq req) {
         return req.getCode() + ", " + req.getName();
     }
+
+    @GetMapping("/send")
+    public String send(String msg) {
+        rabbitmqTemplate.convertAndSend("burning", msg);
+        return "消息： " + msg + "，已发送";
+    }
+
 }
